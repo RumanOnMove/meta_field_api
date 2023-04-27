3e305498a9c5f38eff57ba99c890586df870c263
RumanOnMove <oruman@moveon.com.bd>
2023-04-27 15:43:03 +0600

    Updated engine.rb for loading migration file

--- lib/meta_field_api/engine.rb
+++ lib/meta_field_api/engine.rb
@@ -1,8 +1,13 @@
 module MetaFieldApi
   class Engine < ::Rails::Engine
     isolate_namespace MetaFieldApi
-    config.generators do |g|
-      g.orm :active_record, migration: true
+    # Add this line to load migrations
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match?(root.to_s)
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
     end
   end
 end
