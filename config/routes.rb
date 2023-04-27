9987dc0df6ef040c839211a4f61107d01eab5ca8
RumanOnMove <oruman@moveon.com.bd>
2023-04-27 16:36:12 +0600

    Updated migrated field and meta_field_params methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MetaFieldApi::Engine.routes.draw do
   get 'meta_fields/index'
   get 'meta_fields/show'
-  get 'meta_fields/create'
+  post 'meta_fields/create'
   get 'meta_fields/update'
   get 'meta_fields/destroy'
 end
