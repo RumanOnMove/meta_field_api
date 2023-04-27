e75f4f5d22b8bb7095f8cc6fc4788a8d3311efc6
RumanOnMove <oruman@moveon.com.bd>
2023-04-27 14:58:54 +0600

    Modified engine.rb for automatic genarate model and migration

--- lib/meta_field_api/engine.rb
+++ lib/meta_field_api/engine.rb
@@ -1,5 +1,8 @@
 module MetaFieldApi
   class Engine < ::Rails::Engine
     isolate_namespace MetaFieldApi
+    config.generators do |g|
+      g.orm :active_record, migration: true
+    end
   end
 end
