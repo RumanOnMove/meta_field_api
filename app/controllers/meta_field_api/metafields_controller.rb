04ba33c6b90e71c4e7bb43cc904ebd1d45f2aa8b
RumanOnMove <oruman@moveon.com.bd>
2023-05-07 15:17:29 +0600

    Reinitialize index, create, show, update and destroy method and upgrate there code

--- app/controllers/meta_field_api/metafields_controller.rb
+++ app/controllers/meta_field_api/metafields_controller.rb
@@ -3,35 +3,59 @@ module MetaFieldApi
     # List of all metafields
     def index
       metafields = Metafield.all
-      render json: metafields, status: :ok
+      render json: { metafields: metafields, message: 'Metafields retrieved successfully!' }, status: :ok
     end
 
     # Store metafields
     def create
       metafield = Metafield.new(metafield_params)
+
       if metafield.save
-        render json: metafield, status: :created
+        render json: { metafield: metafield, message: 'Metafield created successfully!' }, status: :created
       else
-        render json: metafield.errors, status: :unprocessable_entity
+        render json: { error: metafield.errors }, status: :unprocessable_entity
+        end
+    rescue StandardError => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
+
+    # Show specific metafield
+    def show
+      metafield = Metafield.find_by(id: params[:id])
+
+      if metafield
+        render json: { metafield: metafield, message: 'Metafield retrieved successfully!' }, status: :ok
+      else
+        render json: { message: 'Metafield not found' }, status: :not_found
       end
     end
 
-    # Show single metafield
-    def show
-      #
-    end
-
-    # Updating met
+    # Update specific metafield
     def update
-      #
+      metafield = Metafield.find_by(id: params[:id])
+
+      if metafield.update(metafield_params)
+        render json: { metafield: metafield, message: 'Metafield updated successfully!' }, status: :ok
+      else
+        render json: { error: metafield.errors }, status: :unprocessable_entity
+      end
+    rescue StandardError => e
+      render json: { error: e.message }
     end
 
     def destroy
-      #
+      metafield = Metafield.find_by(id: params[:id])
+
+      if metafield&.destroy
+        render json: { message: 'Metafield deleted successfully!' }, status: :ok
+      else
+        render json: { error: 'Metafield not found or could not be deleted' }, status: :unprocessable_entity
+      end
     end
 
     private
 
+    # Metafield params
     def metafield_params
       params.permit(:namespace, :key, :value, :description, :my_type, :owner_type, :owner_id)
     end
